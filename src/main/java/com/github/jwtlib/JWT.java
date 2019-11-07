26462bfa519883e39586564ce5014669d6b8a779
Gaurav Sharma <gaurav.cs.sharma@gmail.com>
2019-11-06 17:34:57 -0800

    Some more of the skeleton.

---
+++ src/main/java/com/github/jwtlib/JWT.java
@@ -0,0 +1,24 @@
+package com.github.jwtlib;
+
+/**
+ * JSON Web Tokens (JWT) helper utility.
+ * 
+ * @author gaurav
+ */
+public final class JWT {
+
+  /**
+   * Generate a new JWT token.
+   */
+  public static String generateToken() {
+    // header
+    // payload
+    // signature
+    return null;
+  }
+
+  public static boolean validateToken(final String token) {
+    return false;
+  }
+
+}
